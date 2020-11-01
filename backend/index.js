48d7d1820af73cec4133e78c0ec2047628f08330
Arthur Leung <arcyleung@gmail.com>
2020-11-01 03:47:59 -0500

    Add MOTD template generation

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ const nodemailer = require('nodemailer');
 const Pdfmake = require('pdfmake');
 const moment = require('moment');
 const cron = require('node-cron');
+const path = require('path');
 const { execSync } = require('child_process');
+const Handlebars = require('handlebars');
 require('dotenv').config();
 
 let mailingList;
@@ -28,29 +30,6 @@ try {
   process.exit(1);
 }
 
-// Set up the cron job to read the latest MOTD from the SMTP host
-let lastTimestamp;
-const curlCommand = `curl -s --url 'imap://${process.env.MOTD_HOST}/INBOX;UID=*;SECTION=HEADER.FIELDS%20(DATE)' 'imap://${process.env.MOTD_HOST}/INBOX;UID=*;SECTION=HEADER.FIELDS%20(X-SENDER)' 'imap://${process.env.MOTD_HOST}/INBOX;UID=*;SECTION=TEXT' -u ${process.env.MOTD_USER}:${process.env.MOTD_PASS}`;
-
-cron.schedule('* * * * *', () => {
-  const output = execSync(curlCommand);
-  const [timestamp, sender, message] = output.toString().split(/[\r\n]+/);
-  if (timestamp === lastTimestamp) {
-    return;
-  }
-
-  lastTimestamp = timestamp;
-
-  const senderEmail = sender.split(' ')[1];
-  if (!motdAuthorizedEmailsList.contains(senderEmail)) {
-    return;
-  }
-
-  // Generarte the new MOTD html
-  console.log(sender);
-  console.log(message);
-});
-
 // Refactor into config; we likely won't see > 100 referrals a day
 const maxReferrals = 100;
 let currentRefIx = 0;
@@ -70,6 +49,39 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+});
+
+function renderTemplate(data, templateFile, outputFile) {
+  const source = fs.readFileSync(templateFile,'utf8').toString();
+  const template = Handlebars.compile(source);
+  const output = template(data);
+  fs.writeFileSync(path.join(process.env.FRONTEND_ASSETS_PATH, outputFile), output);
+}
+
+// Set up the cron job to read the latest MOTD from the SMTP host
+let lastTimestamp;
+const curlCommand = `curl -s --url 'imap://${process.env.MOTD_HOST}/INBOX;UID=*;SECTION=HEADER.FIELDS%20(DATE)' 'imap://${process.env.MOTD_HOST}/INBOX;UID=*;SECTION=HEADER.FIELDS%20(X-SENDER)' 'imap://${process.env.MOTD_HOST}/INBOX;UID=*;SECTION=TEXT' -u ${process.env.MOTD_USER}:${process.env.MOTD_PASS}`;
+
+cron.schedule('* * * * *', () => {
+  const output = execSync(curlCommand);
+  const [timestamp, sender, message] = output.toString().split(/[\r\n]+/);
+  if (timestamp === lastTimestamp) {
+    return;
+  }
+
+  lastTimestamp = timestamp;
+
+  const senderEmail = sender.split(' ')[1];
+  if (!motdAuthorizedEmailsList.includes(senderEmail)) {
+    return;
+  }
+
+  // Generarte the new MOTD html
+  console.log(sender);
+  console.log(message);
+
+  renderTemplate({ message }, 'templates/motd.hbs', 'motd.html');
+  console.log(`Generated new MOTD at ${moment.utc()}`);
 });
 
 function createPdfBinary(pdfDoc, callback, data) {
